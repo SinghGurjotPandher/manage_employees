84d7f85df66edc0f499b5db98e6955492aba7b6e
Gurjot Singh Pandher <gurjotp0@gmail.com>
2024-07-26 04:57:44 -0700

    Added more departments and roles data.

--- app/user_setup/department_roles.tsx
+++ app/user_setup/department_roles.tsx
@@ -4,8 +4,17 @@ import React, { useState } from 'react';
 export default function RoleInformation() {
     let departmentRoles = {
         'Select a Department' : ['Select a Department First'],
-        'Quality Assurance': ['Line Inspecter', 'Technician', 'Coordinator','Supervisor','Dock','Manager','Hygienist'],
-        'Operations': ['Team Member', 'Machine Operator'],
+        'Quality Assurance': ['Select a Role','Line Inspecter', 'Technician', 'Coordinator','Supervisor','Dock','Manager','Hygienist'],
+        'Operations': ['Select a Role', 'Team Member', 'Machine Operator','Coordinator','Supervisor','Production Manager','Operations Manager'],
+        'Maintenance': ['Select a Role', 'Receptionist', 'Mechanics','Coordinator','Supervisor','Manager'],
+        'Safety': ['Select a Role', 'Coordinator','Manager','Director'],
+        'Human Resources': ['Select a Role', 'Representative','Manager','Senior Representative'],
+        'Research and Development': ['Select a Role', 'Chemist','Regulatory Specialist','Food Technologist','Manager'],
+        'Lab': ['Select a Role', 'Technician'],
+        'Information Technology': ['Select a Role', 'Desk Helper','Manager'],
+        'Transportation': ['Select a Role', 'Dispatcher','Manager'],
+        'Customer Service': ['Select a Role', 'Representative'],
+        'Scheduling': ['Select a Role', 'Supply Chain Analyst','Manager','Demand Planner']
     };
     
     let departmentNotSelected = Object.keys(departmentRoles)[0];
