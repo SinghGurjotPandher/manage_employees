31fa00bb00d04a18eb8ebedb7198dcc27d87572c
Gurjot Singh Pandher <gurjotp0@gmail.com>
2024-07-27 03:12:20 -0700

    Removed usePathname from layout.tsx -- will be added once deployment error is fixed.

--- app/user_setup/layout.tsx
+++ app/user_setup/layout.tsx
@@ -10,23 +10,12 @@ export default function RootLayout({
   return (
     <html lang="en" >
       <body className="bg-cyan-50">
-        <div className="flex justify-end">             
+        <div className="flex justify-end">  
+                       
             <Link href='/' className="button max-w-sm">
                 Home
             </Link>
-            {
-            usePathname() != '/user_setup/login' &&
-            <Link href='/user_setup/login' className="button max-w-sm">
-                Login
-            </Link>
-            }
-
-            {
-            usePathname() != '/user_setup/register' && usePathname() != '/user_setup/register/successful' &&
-            <Link href='/user_setup/register' className="button max-w-sm">
-                Register
-            </Link>
-            }
+            
         </div>
         {children}
       </body>
